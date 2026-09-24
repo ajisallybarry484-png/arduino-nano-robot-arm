10e0b88cb01f961ac4a7c75c880410601386d2fb
Aji Barry <ajisallybarry484@gmail.com>
2026-09-24 16:55:40 -0400

    Add servo configuration file

---
+++ config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Servo pins
+#define BASE_SERVO_PIN 9
+#define ARM_SERVO_PIN 10
+
+// Base servo limits
+#define BASE_MIN_ANGLE 0
+#define BASE_MAX_ANGLE 180
+
+// Arm servo limits
+#define ARM_MIN_ANGLE 0
+#define ARM_MAX_ANGLE 180
+
+#endif
